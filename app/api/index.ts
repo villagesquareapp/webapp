85072ee87ce5c83252b4e3d00d94d9edf83f4e1c
tosinkoa <paulalbertrichard@gmail.com>
2024-11-26 07:02:27 +0100

    Fix build error (1)

--- app/api/index.ts
+++
@@ -1,6 +0,0 @@
-export { apiGet } from './get'
-export { apiPost } from './post'
-export { apiPut } from './put'
-export { apiDelete } from './delete'
-export { apiUploadFile } from './upload'
-export type { ApiResponse } from './base'
