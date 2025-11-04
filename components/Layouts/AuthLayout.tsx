bd1ec1af19b47551df2b0be4ea93fba94989fff6
Fafiyebipelumi <pelumifafiyebi@gmail.com>
2025-11-04 16:00:59 +0100

    fixes on authentication for production

--- components/Layouts/AuthLayout.tsx
+++ components/Layouts/AuthLayout.tsx
@@ -16,7 +16,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <div className="container relative min-h-[100dvh] flex flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col p-10 lg:flex">
           <div className="flex flex-col items-center justify-center  gap-y-3 my-auto h-[400px]">
-            <Image src="/images/vs_logo.png" alt="Logo" width={150} height={150} />
+            <Image src="/images/vs_logo.png" alt="VS-Logo" width={150} height={150} />
             <div className="flex flex-col items-center justify-center gap-y-1">
               <p className="font-[Ogonek Unicase] font-black text-4xl">VillageSquare</p>
               <p className="font-poppins">Where Connections Flourish.</p>
